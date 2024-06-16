0715aac5c14204cf2c7e89d1d1d5696c27bd739d
OhHaneol <otyvs1109@hs.ac.kr>
2024-06-16 22:05:36 +0900

    feat: change home page structure

--- src/main/resources/templates/script/noteList.js
+++ src/main/resources/templates/script/noteList.js
@@ -1,5 +1,7 @@
-const switchButton = document.getElementById('switch');
+const searchInput = document.getElementById('search-txt');
+const searchButton = document.getElementById('search-btn');
 const noteList = document.getElementById('note-list');
+const switchButton = document.getElementById('switch');
 
 // 초기에 미완성 노트 목록을 가져옵니다.
 fetchNotes(false);
@@ -10,9 +12,37 @@ switchButton.addEventListener('change', (event) => {
     fetchNotes(isCompleted);
 });
 
+// 검색 버튼 클릭 이벤트 리스너
+searchButton.addEventListener('click', () => {
+    const searchKeyword = searchInput.value.trim();
+    if (searchKeyword) {
+        fetchSearchResults(searchKeyword);
+    } else {
+        clearNoteList();
+    }
+});
+
 // 백엔드에서 노트 목록 가져오기
 function fetchNotes(isCompleted) {
     const url = `http://localhost:8080/api/home/notes?status=${isCompleted}`;
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-FP-AUTH-TOKEN': 'eyJhbGciOiJIUzI1NiJ9.eyJuaWNrbmFtZSI6InRlc3RlcjEiLCJ1aWQiOjF9.WNoIH0Tl-tsM6M7uf0YEaOlH8KrmM1ja2o78zs3AHKw'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const notes = data.data;
+        renderNotes(notes);
+    })
+    .catch(error => console.error(error));
+}
+
+// 백엔드에서 검색 결과 가져오기
+function fetchSearchResults(searchKeyword) {
+    const url = `http://localhost:8080/api/home/search?searchKeyword=${encodeURIComponent(searchKeyword)}`;
     fetch(url, {
         method: 'GET',
         headers: {
@@ -38,4 +68,9 @@ function renderNotes(notes) {
         tr.appendChild(td);
         noteList.appendChild(tr);
     });
+}
+
+// 노트 목록 초기화
+function clearNoteList() {
+    noteList.innerHTML = '';
 }
