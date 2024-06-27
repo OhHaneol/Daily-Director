69c580a3492c09721ed6114e8e191cfd5a4139ee
OhHaneol <otyvs1109@hs.ac.kr>
2024-06-27 09:53:29 +0900

    feat: add crossorigin

--- src/main/java/ohahsis/dailydirecter/auth/presentation/AuthController.java
+++ src/main/java/ohahsis/dailydirecter/auth/presentation/AuthController.java
@@ -11,11 +11,9 @@ import ohahsis.dailydirecter.auth.dto.response.AuthLoginResponse;
 import ohahsis.dailydirecter.common.model.ResponseDto;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:63342")
 @Tag(name = "인증,인가 관리")
 @RestController
 @RequestMapping(path = "/api/auth", produces = MediaType.APPLICATION_JSON_VALUE)
