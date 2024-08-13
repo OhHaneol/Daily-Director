627db1a2bcf9db745b317b00eb87cd4da4bc02fa
OhHaneol <otyvs1109@hs.ac.kr>
2024-08-13 16:14:19 +0900

    feat: add login redirect

--- src/main/java/ohahsis/dailydirecter/config/web/WebConfig.java
+++ src/main/java/ohahsis/dailydirecter/config/web/WebConfig.java
@@ -1,10 +1,12 @@
 package ohahsis.dailydirecter.config.web;
 
 import lombok.RequiredArgsConstructor;
+import ohahsis.dailydirecter.config.interceptor.AuthInterceptor;
 import ohahsis.dailydirecter.config.resolver.UserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,6 +16,7 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final UserArgumentResolver userArgumentResolver;
+    private final AuthInterceptor authInterceptor;
 
     // HandlerMethodArgumentResolver 등록을 위해서는 WebMvcConfigurer 를 구현한 클래스가 필요하다.
     // 그리고 addArgumentResolvers() 라는 메소드를 구현해야 한다.
@@ -21,6 +24,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/html/user/login", "/html/user/signUp", "/css/**", "/script/**", "/img/**");
     }
 
     @Override
